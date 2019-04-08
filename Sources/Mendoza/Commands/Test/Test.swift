6f1d43d17e7e642451c9f0e413e8b502c57fc53b
Tomas Camin <tomas.camin@schibsted.com>
2019-04-08 20:14:02 +0200

    Improve error message

--- Sources/Mendoza/Commands/Test/Test.swift
+++ Sources/Mendoza/Commands/Test/Test.swift
@@ -189,7 +189,7 @@ class Test {
                     op.logger.log(exception: opError.localizedDescription)
                 }
                 
-                print("\n💥 \(op.className.components(separatedBy: ".").last ?? op.className) did throw exception\n")
+                print("\n💥 \(op.className.components(separatedBy: ".").last ?? op.className) did throw exception, see session logs for details on what went wrong\n")
                 
                 self.tearDown(operations: operations, testSessionResult: testSessionResult, error: opError as? Error)
             }
