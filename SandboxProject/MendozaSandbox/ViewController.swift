e5e67cd49e074f7c91e43ae17099f0d3c394f9fd
Tomas Camin <tomas.camin@adevinta.com>
2019-07-07 14:24:45 +0200

    Add sandbox project

---
+++ SandboxProject/MendozaSandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MendozaSandbox
+//
+//  Created by tomas on 07/07/2019.
+//  Copyright © 2019 tomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SandboxProject/MendozaSandboxUITests/MendozaSandboxUITests.swift
@@ -0,0 +1,19 @@
+//
+//  MendozaSandboxUITests.swift
+//  MendozaSandboxUITests
+//
+//  Created by tomas on 07/07/2019.
+//  Copyright © 2019 tomas. All rights reserved.
+//
+
+import XCTest
+
+class MendozaSandboxUITests: XCTestCase {
+
+    override func setUp() {
+        XCUIApplication().launch()
+    }
+
+    //func testExample1() { XCTAssert(true) }
+    func testExampleFail() { XCTAssert(false) }
+}
