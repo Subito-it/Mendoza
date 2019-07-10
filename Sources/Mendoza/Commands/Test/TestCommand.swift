004d0f4e4adc272ab90f8ba81de398524a0a14a6
Tomas Camin <tomas.camin@adevinta.com>
2019-07-10 20:49:31 +0200

    Rename

--- Sources/Mendoza/Commands/Test/TestCommand.swift
+++ Sources/Mendoza/Commands/Test/TestCommand.swift
@@ -13,14 +13,14 @@ class TestCommand: Command {
     let usage: String? = "Dispatch UI tests as specified in the `configuration_file`"
     let help: String? = "Dispatch UI tests"
     
-    let debugPlugins = Flag(short: nil, long: "plugin_debug", help: "Dump plugin invocation commands")
-    let dispatchOnLocalHost = Flag(short: "l", long: "use_localhost", help: "Distribute tests on localhost as well")
+    let debugPluginsFlag = Flag(short: nil, long: "plugin_debug", help: "Dump plugin invocation commands")
+    let dispatchOnLocalHostFlag = Flag(short: "l", long: "use_localhost", help: "Distribute tests on localhost as well")
     
-    let configuration = Argument<URL>(name: "configuration_file", kind: .positional, optional: false, help: "Mendoza's configuration file path", autocomplete: .files("json"))
-    let includePattern = Argument<String>(name: "files", kind: .named(short: "f", long: "include_files"), optional: true, help: "Specify from which files UI tests should be extracted. Accepts wildcards and comma separated. e.g SBTA*.swift,SBTF*.swift. Default: '*.swift'", autocomplete: .files("swift"))
-    let excludePattern = Argument<String>(name: "files", kind: .named(short: "x", long: "exclude_files"), optional: true, help: "Specify which files should be skipped when extracting UI tests. Accepts wildcards and comma separated. e.g SBTA*.swift,SBTF*.swift. Default: ''", autocomplete: .files("swift"))
-    let deviceName = Argument<String>(name: "name", kind: .named(short: "d", long: "device_name"), optional: true, help: "Device name to use to run tests. e.g. 'iPhone 8'")
-    let deviceRuntime = Argument<String>(name: "version", kind: .named(short: "v", long: "device_runtime"), optional: true, help: "Device runtime to use to run tests. e.g. '12.1'")
+    let configurationPathField = Argument<URL>(name: "configuration_file", kind: .positional, optional: false, help: "Mendoza's configuration file path", autocomplete: .files("json"))
+    let includePatternField = Argument<String>(name: "files", kind: .named(short: "f", long: "include_files"), optional: true, help: "Specify from which files UI tests should be extracted. Accepts wildcards and comma separated. e.g SBTA*.swift,SBTF*.swift. Default: '*.swift'", autocomplete: .files("swift"))
+    let excludePatternField = Argument<String>(name: "files", kind: .named(short: "x", long: "exclude_files"), optional: true, help: "Specify which files should be skipped when extracting UI tests. Accepts wildcards and comma separated. e.g SBTA*.swift,SBTF*.swift. Default: ''", autocomplete: .files("swift"))
+    let deviceNameField = Argument<String>(name: "name", kind: .named(short: "d", long: "device_name"), optional: true, help: "Device name to use to run tests. e.g. 'iPhone 8'")
+    let deviceRuntimeField = Argument<String>(name: "version", kind: .named(short: "v", long: "device_runtime"), optional: true, help: "Device runtime to use to run tests. e.g. '12.1'")
     let timeoutField = Argument<Int>(name: "minutes", kind: .named(short: nil, long: "timeout"), optional: true, help: "Maximum allowed time (in minutes) before dispatch process is automatically terminated")
     let pluginCustomField = Argument<String>(name: "data", kind: .named(short: nil, long: "plugin_data"), optional: true, help: "A custom string that can be used to inject data to plugins")
     let failingTestsRetryCountField = Argument<Int>(name: "count", kind: .named(short: "r", long: "failure_retry"), optional: true, help: "Number of times a failing tests should be repeated")
@@ -28,23 +28,23 @@ class TestCommand: Command {
     func run() -> Bool {
         do {
             let device: Device
-            if let deviceName = deviceName.value, let deviceRuntime = deviceRuntime.value {
+            if let deviceName = deviceNameField.value, let deviceRuntime = deviceRuntimeField.value {
                 device = Device(name: deviceName, runtime: deviceRuntime)
             } else {
                 device = Device.defaultInit()
             }
             let timeout = timeoutField.value ?? 180
-            let filePatterns = FilePatterns(commaSeparatedIncludePattern: includePattern.value, commaSeparatedExcludePattern: excludePattern.value)
+            let filePatterns = FilePatterns(commaSeparatedIncludePattern: includePatternField.value, commaSeparatedExcludePattern: excludePatternField.value)
             let failingTestsRetryCount = failingTestsRetryCountField.value ?? 0
 
-            let test = try Test(configurationUrl: configuration.value!,
+            let test = try Test(configurationUrl: configurationPathField.value!,
                                 device: device,
                                 filePatterns: filePatterns,
                                 timeoutMinutes: timeout,
                                 failingTestsRetryCount: failingTestsRetryCount,
-                                dispatchOnLocalHost: dispatchOnLocalHost.value,
+                                dispatchOnLocalHost: dispatchOnLocalHostFlag.value,
                                 pluginData: pluginCustomField.value,
-                                debugPlugins: debugPlugins.value)
+                                debugPlugins: debugPluginsFlag.value)
             
             test.didFail = { [weak self] in self?.handleError($0) }
             try test.run()
