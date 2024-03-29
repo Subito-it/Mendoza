3ca03c9dc89eb97902ecfe4a8a4b10563f3ade80
Tomas Camin <tomas.camin@adevinta.com>
2024-03-29 10:26:53 +0100

    Improve test logic extraction

--- Sources/mendoza/Models/KittenElement.swift
+++ Sources/mendoza/Models/KittenElement.swift
@@ -8,12 +8,22 @@
 import Foundation
 
 struct KittenElement: Codable, Equatable, Hashable {
+    struct Attribute: Codable, Equatable, Hashable {
+        let attribute: String?
+
+        enum CodingKeys: String, CodingKey {
+            case attribute = "key.attribute"
+        }
+    }
+
     let stage: String?
     let accessibility: String?
     let types: [KittenElement]?
     let name: String?
     let kind: String?
     let subElements: [KittenElement]?
+    let attributes: [KittenElement.Attribute]?
+    let typeName: String?
 
     enum CodingKeys: String, CodingKey {
         case stage = "key.diagnostic_stage"
@@ -22,13 +32,17 @@ struct KittenElement: Codable, Equatable, Hashable {
         case name = "key.name"
         case kind = "key.kind"
         case subElements = "key.substructure"
+        case attributes = "key.attributes"
+        case typeName = "key.typename"
     }
 
     var isTestMethod: Bool {
         name?.hasPrefix("test") == true &&
             name?.hasSuffix("()") == true &&
             accessibility != "source.lang.swift.accessibility.private" &&
-            kind == "source.lang.swift.decl.function.method.instance"
+            kind == "source.lang.swift.decl.function.method.instance" &&
+            !(attributes?.contains(where: { $0.attribute == "source.decl.attribute.override" }) == true) &&
+            typeName == nil
     }
 
     var isOpenClass: Bool {
--- Sources/mendoza/XCTest/XCTestFileParser.swift
+++ Sources/mendoza/XCTest/XCTestFileParser.swift
@@ -10,33 +10,35 @@ import SourceKittenFramework
 
 struct XCTestFileParser {
     func extractTestCases(from urls: [URL]) throws -> [TestCase] {
-        var result = [TestCase]()
+        var testClasses = [KittenElement(stage: nil, accessibility: nil, types: nil, name: "XCTestCase", kind: nil, subElements: nil, attributes: nil, typeName: nil)]
+        for _ in 0 ..< 5 { // Repeat to collect class inheritance
+            for url in urls {
+                guard let file = File(path: url.path) else { throw Error("File `\(url.path)` does not exists") }
 
-        for url in urls {
-            guard let file = File(path: url.path) else { throw Error("File `\(url.path)` does not exists") }
+                let structure = try Structure(file: file).description
+                guard let structureData = structure.data(using: .utf8) else { throw Error("Failed parsing `\(url.path)` source file") }
+                let parsed = try JSONDecoder().decode(KittenElement.self, from: structureData)
 
-            let structure = try Structure(file: file).description
-            guard let structureData = structure.data(using: .utf8) else { throw Error("Failed parsing `\(url.path)` source file") }
-            let parsed = try JSONDecoder().decode(KittenElement.self, from: structureData)
-
-            guard let visibleClasses = parsed.subElements?.filter(\.isOpenClass) else {
-                return [] // no testing classes found
-            }
-
-            let testClasses = visibleClasses.filter { $0.conforms(candidates: visibleClasses).contains("XCTestCase") }
-
-            let testCases: [[TestCase]] = testClasses.compactMap {
-                guard let suite = $0.name,
-                      let methods = $0.subElements?.filter(\.isTestMethod)
-                else {
-                    return nil
+                guard let visibleClasses = parsed.subElements?.filter(\.isOpenClass) else {
+                    return [] // no testing classes found
                 }
 
-                return methods.map { TestCase(name: $0.name!.replacingOccurrences(of: "()", with: ""), suite: suite) } // swiftlint:disable:this force_unwrapping
+                testClasses += visibleClasses.filter { visibleClass in testClasses.contains(where: { visibleClass.conforms(candidates: visibleClasses).contains($0.name ?? "") }) }
+                testClasses = testClasses.uniqued()
+            }
+        }
+
+        let testCases: [[TestCase]] = testClasses.compactMap {
+            guard let suite = $0.name,
+                  let methods = $0.subElements?.filter(\.isTestMethod)
+            else {
+                return nil
             }
 
-            result += testCases.flatMap { $0 }
+            return methods.map { TestCase(name: $0.name!.replacingOccurrences(of: "()", with: ""), suite: suite) } // swiftlint:disable:this force_unwrapping
         }
+
+        let result = testCases.flatMap { $0 }.sorted(by: { $0.testIdentifier < $1.testIdentifier })
 
         return result
     }
